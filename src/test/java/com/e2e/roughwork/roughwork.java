5d0ae539f3bc7640bff52c26a753ccc2ce7a45ec
Kanwarpreet Singh Khurana <Kanwarpreet_K@ad.infosys.com>
2017-07-18 03:48:53 +0530

    update few files

--- src/test/java/com/e2e/roughwork/roughwork.java
+++ src/test/java/com/e2e/roughwork/roughwork.java
@@ -31,20 +31,23 @@ public class roughwork {
 	public static Xls_Reader excel = new Xls_Reader("D:\\Selenium Practise\\testdata.xlsx");	
 	static String path = "D:\\Selenium Practise\\testdata.xlsx";
 	
-	@SuppressWarnings("deprecation")
+
 	public static void main(String[] args) throws UnknownHostException, AddressException, MessagingException {
-		monitoringMail mail = new monitoringMail();
+		//monitoringMail mail = new monitoringMail();
+		String Tcase = "Login Test";
+		String Hyperlink = "http://"+Inet4Address.getLocalHost().getHostAddress()+":8080/job/E2E_GitHub_Integration/HTML_Report/"+Tcase +"Test.html";
+		excel.addHyperLink("test_suite", "Result", "LoginTest", 1, Hyperlink, "click");
 		/*String Tcase = "Login Test";
 		String Hyperlink = System.getProperty("user.dir")+"\\target\\surefire-reports\\E2E POM Suite\\"+Tcase +" Test.html";
 		String A ="https://stackoverflow.com/questions/39303592/how-to-add-space-address-of-hyperlink-in-java-apche";
 		excel.addHyperLink("test_suite", "ReportLink", "LoginTest", 1, A, "Hello");
 		excel.setCellData("test_suite", "ReportLink", 4, "Hello", Hyperlink);*/
-		String Tcase = "Login Test";
-		String Hyperlink = "http://"+Inet4Address.getLocalHost().getHostAddress()+":8080/job/E2E_POM_Jenkin_Execution/Extent_Report/";
-		System.out.println(Hyperlink);	
-		System.out.println(InetAddress.getLocalHost().getHostAddress());
-		String messgaeBody = Hyperlink;
-		excel.addHyperLink("test_suite", "Result", "LoginTest", 1, Hyperlink, "Hello");
-		mail.sendMail(TestConfig.server, TestConfig.from, TestConfig.to, TestConfig.subject, messgaeBody);
+		//String Tcase = "Login Test";
+		//String Hyperlink = "http://"+Inet4Address.getLocalHost().getHostAddress()+":8080/job/E2E_POM_Jenkin_Execution/Extent_Report/";
+		//System.out.println(Hyperlink);	
+		//System.out.println(InetAddress.getLocalHost().getHostAddress());
+		//String messgaeBody = Hyperlink;
+		//excel.addHyperLink("test_suite", "Result", "LoginTest", 1, Hyperlink, "Hello");
+		//mail.sendMail(TestConfig.server, TestConfig.from, TestConfig.to, TestConfig.subject, messgaeBody);
 	}
 }
